2ae0fccd344215103eb84c9380c8e3cdcd10a92f
Arom Kim <arom_kim@naver.com>
2020-08-25 22:04:14 +0900

    hashchange 구현

--- src/components/header.js
+++ src/components/header.js
@@ -1,50 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 
-const Header = ({ menuList, background }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            logo
-            title
+const Header = ({ menuList, background }) => {
+  const [hash, setHash] = useState(null)
+
+  useEffect(() => {
+    function handleActiveMenu () {
+      setHash(window.location.hash.split('#')[1])
+    }
+    window.addEventListener('hashchange', handleActiveMenu)
+    return () => window.removeEventListener('hashchange', handleActiveMenu)
+  })
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              logo
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <header style={{ background: `${background ? '#fff' : null}` }}>
-        <h1>
-          <Link to={window.location.hash ? null : '/'}>
-            <img
-              src={`/${data.site.siteMetadata.logo}`}
-              alt={data.site.siteMetadata.title}
-            />
-          </Link>
-        </h1>
-        <nav id="menu">
-          <ul>
-            <Link activeClassName="active" to="/all">
-              <li>ALL</li>
+      `}
+      render={data => (
+        <header style={{ background: `${background ? '#fff' : null}` }}>
+          <h1>
+            <Link to={window.location.hash ? null : '/'}>
+              <img
+                src={`/${data.site.siteMetadata.logo}`}
+                alt={data.site.siteMetadata.title}
+              />
             </Link>
-          </ul>
-          <ul>
-            {menuList.map(menu => (
-              <Link
-                activeClassName="active"
-                partiallyActive={true}
-                key={menu.index}
-                to={`/${menu.category}`}
-              >
-                <li>{menu.category.toUpperCase()}</li>
+          </h1>
+          <nav>
+            <ul>
+              <Link activeClassName="active" to="/all">
+                <li>ALL</li>
               </Link>
-            ))}
-          </ul>
-        </nav>
-      </header>
-    )}
-  />
-)
+            </ul>
+            <ul>
+              {menuList.map(menu => (
+                <Link
+                  activeClassName="active"
+                  className={`${hash === menu.category ? 'active' : ''}`}
+                  partiallyActive={true}
+                  key={menu.index}
+                  to={`/${menu.category}`}
+                >
+                  <li>{menu.category.toUpperCase()}</li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        </header>
+      )}
+    />
+  )
+}
 
 export default Header
